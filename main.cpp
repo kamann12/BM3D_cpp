a267ec90df9d7948df216e1b7f0fdb8484f3f858
kamann12 <kksi34129147@gmail.com>
2025-09-30 01:12:45 +0900

    gtを削除

--- main.cpp
+++ main.cpp
@@ -5,19 +5,6 @@
 #include "cbm3d_wiener.h"
 using namespace std;
 
-double get_psnr(ImageType *img1, ImageType *img2, int pixels, ImageType vmax)
-{
-	double mse = 0;
-	double diff;
-	for (int i = 0; i < pixels; i++)
-	{
-		diff = (double)img1[i] - (double)img2[i];
-		mse += diff * diff;
-	}
-	mse /= pixels;
-	return (10 * log10((double)vmax * vmax / mse));
-}
-
 FILE *openfile(const char *fname, const char *mode)
 {
 	FILE *f = fopen(fname, mode);
@@ -32,18 +19,13 @@ FILE *openfile(const char *fname, const char *mode)
 int main()
 {
 	int w = 512, h = 512;
-	int chnl = 3;			// YUV 4:0:0 or 4:4:4
+	int chnl = 1;			// YUV 4:0:0 or 4:4:4
 
-	int en_bm3d_step2 = 1;	// enable step2 of bm3d
+	int en_bm3d_step2 = 0;	// enable step2 of bm3d
 	int sigma_step1 = 36;	// here same for Y/U/V, can be different
 	int sigma_step2 = 25;	// bigger for smoother, usually a little smaller than step1
 
 	int frames = 1;		// frames to process
-
-	// ground truth
-	FILE *gtf = openfile("test/yuv444_512x512_lena_gt.yuv", "rb");
-	ImageType *gt = new ImageType[w * h * chnl];
-	fread(gt, sizeof(ImageType), w * h * chnl, gtf);
 
 	// noisy input and denoised output
 	FILE *inf = openfile("test/yuv444_512x512_lena.yuv", "rb");
@@ -87,9 +69,6 @@ int main()
 		denoiser->load(noisy, sigma_step1);
 		denoiser->run(clean);
 
-		cout << "noisy PSNR: "    << get_psnr(noisy, gt, w*h*chnl, 255) << "    "
-			 << "denoised PSNR: " << get_psnr(clean, gt, w*h*chnl, 255) << endl;
-
 		fwrite(clean, sizeof(ImageType), w * h * chnl, ouf);
 
 		if (en_bm3d_step2)
@@ -97,9 +76,6 @@ int main()
 			// wiener filtering
 			denoiser_wie->load(noisy, clean, sigma_step2);
 			denoiser_wie->run(clean);
-
-			cout << "noisy PSNR: " << get_psnr(noisy, gt, w * h * chnl, 255) << "    "
-				<< "wiener denoised PSNR: " << get_psnr(clean, gt, w * h * chnl, 255) << endl;
 
 			fwrite(clean, sizeof(ImageType), w * h * chnl, ouf);
 		}
@@ -110,9 +86,6 @@ int main()
 
 	delete denoiser;
 	delete denoiser_wie;
-
-	fclose(gtf);
-	delete[] gt;
 
 	fclose(inf);
 	fclose(ouf);
